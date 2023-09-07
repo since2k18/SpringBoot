309a8c68c1adcfef88494a246db150f908029621
MD Arsath <103190460+since2k18@users.noreply.github.com>
2023-09-07 08:18:14 +0530

    Create MovieListApplication.java

---
+++ MovieListApplication.java
@@ -0,0 +1,13 @@
+package com.instanotes.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieListApplication.class, args);
+	}
+
+}
